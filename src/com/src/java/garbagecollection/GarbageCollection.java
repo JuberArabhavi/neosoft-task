936e6430ff447e0ead8b034311b66285ecd62eb7
juber.arabhavi <juber.arabhavi@neosoftmail.com>
2023-08-03 13:42:51 +0530

    1. Exception Handling for different cases for checked exceptions 2. Create custom exception (checked and unchecked) 3. How to make object eligible for garbage collection - DONE (PUSHED) 4. Multithreading     Create thread (using runnable, thread), create daemon thread, Use notify,notify all,sleep,wait,use thread local variable,use volatile variable, Synchronization and semaphore sample

---
+++ src/com/src/java/garbagecollection/GarbageCollection.java
@@ -0,0 +1,56 @@
+package com.src.java.garbagecollection;
+
+/*
+ * Created By @author : Juber
+ * */
+public class GarbageCollection {
+
+	/**
+	 * 
+	 * 1. By Nullifying the reference variable
+	 * 
+	 * */
+	static GarbageCollection gc = new GarbageCollection();
+	static GarbageCollection gc1 = new GarbageCollection();
+	
+	
+	/*
+	 * 
+	 * 2. By re-assigning the reference variable
+	 * 
+	 * */
+	static GarbageCollection gc2 = new GarbageCollection();
+	static GarbageCollection gc3 = new GarbageCollection();
+	
+	
+	/*
+	 * 3. By creating objects inside a method.
+	 * 
+	 * */
+	static void makeGCEligible() {
+		GarbageCollection gc4 = new GarbageCollection();
+		GarbageCollection gc5 = new GarbageCollection();
+	}
+	
+	public static void main(String[] args) {
+		
+		/**
+		 * 
+		 * 1. By Nullifying the reference variable
+		 * 
+		 * */
+		gc = null;
+		gc1 = null;
+		
+		/*
+		 * 2. By re-assigning the reference variable
+		 * */
+		gc3 = gc2;
+		
+		
+		/*
+		 * 3. By creating objects inside a method
+		 * */
+		makeGCEligible();
+	}
+}
